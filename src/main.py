ab4fdb66ec0506e4000cd7a7bb7356f896b0806f
Frost <nafrost@ebay.com>
2022-04-21 00:39:02 +0300

    update

--- src/main.py
+++ src/main.py
@@ -16,10 +16,30 @@ provenance_path = path/"provenance"
 provenance_path.mkdir(parents=True, exist_ok=True)
 
 
+def close_con_and_cur(con, cur):
+    if cur is not None:
+        cur.close()
+    if con is not None:
+        con.close()
+
+
 def get_postgres_connection(db="amazon"):
     con_args = 'dbname={}'
     con_args = con_args.format(db)
     return psycopg2.connect(con_args)
+
+
+def get_connection_and_cursor(db="amazon"):
+    con, cur = None, None
+    try:
+        con = get_postgres_connection(db)
+        cur = con.cursor()
+        cur.execute("SET search_path TO public, provsql")
+    except Exception as e:
+        close_con_and_cur(con, cur)
+        raise e
+
+    return con, cur
 
 
 def fetch_facts_data(table_name):
@@ -27,13 +47,9 @@ def fetch_facts_data(table_name):
     query = "select * from %s;" % table_name
 
     ans = None
-    con = None
-    cur = None
+    con, cur = None, None
     try:
-        con = get_postgres_connection()
-        cur = con.cursor()
-
-        cur.execute("SET search_path TO public, provsql")
+        con, cur = get_connection_and_cursor()
         cur.execute(query)
         res = cur.fetchall()
 
@@ -48,12 +64,49 @@ def fetch_facts_data(table_name):
             for row in res
         }
     finally:
-        if cur is not None:
-            cur.close()
-        if con is not None:
-            con.close()
+        close_con_and_cur(con, cur)
 
     return ans
+
+
+@app.route("/query/<string:query>/")
+def execute_query(query):
+    """
+    Execute an SQL query and return a list of results.
+    None will be returned in the case of error during the query execution.
+
+    :param query: is an SQL query.
+    :return: list the SQL output tuples.
+    """
+    if query is None:
+        return {ERROR: "Query is empty"}
+
+    app.logger.debug("*** execute_query: processing \"%s\"" % query)
+
+    ans = {ERROR: "Error during query processing"}
+    con, cur = None, None
+    try:
+        con, cur = get_connection_and_cursor()
+
+        app.logger.debug("*** execute_query: got cursor")
+
+        cur.execute(query)
+        res = cur.fetchall()
+
+        app.logger.debug("*** execute_query: fetched %d results" % len(res))
+
+        schema = [desc[0] for desc in cur.description]
+        ans = {
+            "schema": schema[:-1],
+            "outputs": {x[-1]: x[:-1] for x in res}
+        }
+    except Exception as e:
+        app.logger.error("*** execute_query: %s" % str(e))
+    finally:
+        close_con_and_cur(con, cur)
+
+    return ans
+
 
 
 def hash_dir_structure(provenance_hash):
@@ -65,9 +118,21 @@ def hash_dir_structure(provenance_hash):
 
 
 def export_provenance(cur, provenance_hash):
+
+    app.logger.debug("*** export_provenance: export provenance for \"%s\"" % provenance_hash)
+
+    cur_path = provenance_path / ('/'.join(hash_dir_structure(provenance_hash)))
+    if cur_path.exists():
+        app.logger.debug("*** export_provenance: \"%s\" provenance already exist" % provenance_hash)
+        return
+
+    cur_path.mkdir(parents=True, exist_ok=True)
+
     cur.execute("SELECT * FROM sub_circuit_for_where('%s')" % provenance_hash)
     res = cur.fetchall()
     provenance = pd.DataFrame(res, columns=["f", "t", "gate_type", "table_name", "nb_columns", "infos", "tuple_no"])
+
+    app.logger.debug("*** export_provenance: \"%s\" provenance contain %d gates" % (provenance_hash, provenance.shape[0]))
 
     G = nx.DiGraph()
     gates = {}
@@ -81,6 +146,8 @@ def export_provenance(cur, provenance_hash):
     gates_order = list(nx.topological_sort(G))
     for gate_id, gate_hash in enumerate(gates_order):
         gates[gate_hash]["id"] = gate_id + 1
+
+    app.logger.debug("*** export_provenance: traversed \"%s\" provenance" % provenance_hash)
 
     clauses = [[1]]
     for gate in gates_order:
@@ -102,11 +169,10 @@ def export_provenance(cur, provenance_hash):
                 cur_clause.append(in_gate_id)
             clauses.append(cur_clause)
 
-    cur_path = provenance_path / ('/'.join(hash_dir_structure(provenance_hash)))
-    cur_path.mkdir(parents=True, exist_ok=True)
-
     provenance["f_id"] = provenance.f.apply(lambda gate: gates[gate]["id"])
     provenance.to_csv(cur_path / "provenance_as_a_table.csv")
+
+    app.logger.debug("*** export_provenance: \"%s\" table was written to %s" % (provenance_hash, str(cur_path / "provenance_as_a_table.csv")))
 
     with open(cur_path / "circuit", "w") as f:
         f.write("p cnf %d %d\n" % (len(gates), len(clauses)))
@@ -115,53 +181,12 @@ def export_provenance(cur, provenance_hash):
                 f.write("%d " % x)
             f.write("0\n")
 
+    app.logger.debug("*** export_provenance: \"%s\" circuit was written to %s" % (provenance_hash, str(cur_path / "circuit")))
+
     with open(cur_path / ("gates.json"), 'w') as f:
         json.dump(gates, f, indent=4)
 
-
-@app.route("/query/<string:query>/")
-def execute_query(query):
-    """
-    Execute an SQL query and return a list of results.
-    None will be returned in the case of error during the query execution.
-
-    :param query: is an SQL query.
-    :return: list the SQL output tuples.
-    """
-    if query is None:
-        return {ERROR: "Query is empty"}
-
-    app.logger.debug("*** execute_query: processing \"%s\"" % query)
-
-    con = None
-    cur = None
-    ans = {ERROR: "Error during query processing"}
-    try:
-        con = get_postgres_connection()
-        cur = con.cursor()
-
-        app.logger.debug("*** execute_query: got cursor")
-
-        cur.execute("SET search_path TO public, provsql")
-        cur.execute(query)
-        res = cur.fetchall()
-
-        app.logger.debug("*** execute_query: fetched %d results" % len(res))
-
-        schema = [desc[0] for desc in cur.description]
-        ans = {
-            "schema": schema[:-1],
-            "outputs": {x[-1]: x[:-1] for x in res}
-        }
-    except Exception as e:
-        app.logger.error("*** execute_query: %s" % str(e))
-    finally:
-        if cur is not None:
-            cur.close()
-        if con is not None:
-            con.close()
-
-    return ans
+    app.logger.debug("*** export_provenance: \"%s\" gates were written to %s" % (provenance_hash, str(cur_path / "gates.json")))
 
 
 def get_output_tuple_provenance(output_tuple):
@@ -180,7 +205,11 @@ def get_contributing_facts(output_tuples):
     if output_tuples is None:
         return {ERROR: "Output tuples are empty"}
 
+    app.logger.debug("*** get_contributing_facts: got output tuples \"%s\"" % output_tuples)
+
     output_tuples = {f.strip(): [0] for f in output_tuples.split(",")}
+
+    app.logger.debug("*** get_contributing_facts: parsed to %d tuples" % len(output_tuples))
 
     if len(output_tuples) == 0:
         return {ERROR: "Output tuples are empty"}
