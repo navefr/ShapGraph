eafa254757033a4dc58a314f9a419b8227719ac8
Frost <nafrost@ebay.com>
2022-04-28 16:57:44 +0300

    update running query

--- data/running_query.sql
+++ data/running_query.sql
@@ -14,16 +14,17 @@ JOIN category
 ON bought_items.asin = category.asin
 WHERE (
         source_items.main_cat = 'Computers'
+        OR source_items.main_cat = 'Video Games'
         OR source_items.main_cat = 'All Electronics'
     )
     AND (
-        LOWER(feature.value) LIKE '%mb%'
-        OR LOWER(feature.value) LIKE '%memory%'
+        LOWER(feature.value) LIKE '%memory%'
+        OR LOWER(feature.value) LIKE '%mb%'
+        OR LOWER(feature.value) LIKE '%gb%'
     )
-    AND review1.overall >= 4
+    AND review1.reviewerid != review2.reviewerid
+    AND review1.overall >= 3
     AND review1.vote > 0
-    AND review1.verified = 1
-    AND review2.overall >= 4
+    AND review2.overall >= 3
     AND review2.vote > 0
-    AND review2.verified = 1
 GROUP BY category.value
